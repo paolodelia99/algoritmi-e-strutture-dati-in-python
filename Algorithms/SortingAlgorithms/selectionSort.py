12f12f80cc7964320d74c907895ef92924f776bc
Paolo D'Elia <paolo.delia99@gmail.com>
2020-02-28 21:06:31 +0100

    Working on selection sort

--- Algorithms/SortingAlgorithms/selectionSort.py
+++ Algorithms/SortingAlgorithms/selectionSort.py
@@ -1,15 +1,15 @@
 # fixme: metodo sbagliato
 def selecion_sort(list_):
-    """
-    Implementazione di selection sort
+    """Implementazione di selection sort
 
     Parametri
         list_ (list): lista di numeri interi da ordinare
     """
     for j in range(0, len(list_) - 1):
-        currentMin = j
+        current_min = j
         for i in range(j + 1, len(list_)):
-            if list_[i] < list_[currentMin]:
-                currentMin = i
-            if currentMin != j:
-                list_[j], list_[currentMin] = list_[currentMin], list_[j]
+            if list_[i] < list_[current_min]:
+                current_min = i
+            if current_min != j:
+                # scambio il numero al l'indice j con il numero all'indice current_min
+                list_[j], list_[current_min] = list_[current_min], list_[j]
